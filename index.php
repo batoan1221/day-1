3409b326a422491f7f6e667640d5215fae94e5be
batoan <batoan1221@gmail.com>
2013-10-26 15:30:17 +0700

    Connect to Database

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-	echo "Hello world asadsadas"; 
+	// Create connection
+	$con=mysqli_connect("localhost","root","","day1");
+
+	// Check connection
+	if (mysqli_connect_errno($con))
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	else
+	{
+		echo "Connect successfully";
+	}
 ?>
