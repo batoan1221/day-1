1b810cf69d219ce07693f1c86914e6d231388e6b
batoan <batoan1221@gmail.com>
2013-10-26 23:58:03 +0700

    Complete Login form

---
+++ login.php
@@ -0,0 +1,10 @@
+<h1>Login Page</h1>
+<form>
+Username: <input type="text" name="txtUsername"> <br>
+Password: <input type="password" name="txtPassword"> <br>
+<input type="submit" name="btnSubmit" value="Log in">
+<input type="reset" name="btnReset" value="Clear">
+</form>
+<?php
+	
+?>
