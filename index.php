d3646b1c6fdb6c922fbb77d0e1fedc90128ab1b2
batoan <batoan1221@gmail.com>
2013-10-26 15:55:39 +0700

    Finish to insert message to database

--- index.php
+++ index.php
@@ -16,8 +16,16 @@
 
 		return $con;
 	};
-
+	
+	function insertMessageToDatabase($con, $message)
+	{
+		mysqli_query($con, "INSERT INTO message (message_content) VALUES ('".$message."')");
+	}
+	
 	$con = connectToDatabase("localhost","day1","root","");
+
+	// insertMessageToDatabase($con,"message 3");
+
 	if ($con) {
 		$result = mysqli_query($con,"Select * from message");
 		while($row = mysqli_fetch_array($result))
@@ -28,4 +36,5 @@
 	}
 
 	mysqli_close($con);
+
 ?>
