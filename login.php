9ac30c0b31c03f39f53b19894b1ca3dd79e670c6
batoan <batoan1221@gmail.com>
2013-10-27 00:33:25 +0700

    Finish Login form

--- login.php
+++ login.php
@@ -6,5 +6,44 @@ Password: <input type="password" name="txtPassword"> <br>
 <input type="reset" name="btnReset" value="Clear">
 </form>
 <?php
-	
+	function connectToDatabase( $hostName, $databaseName, $username, $password)
+	{
+		// Create connection
+		$con=mysqli_connect($hostName, $username, $password, $databaseName);
+
+		// Check connection
+		if (mysqli_connect_errno($con))
+		{
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+		else
+		{
+			// echo "Connect successfully";
+		}
+
+		return $con;
+	};
+
+	$con = connectToDatabase("localhost","day1","root","");
+
+	if (!empty($_GET['txtUsername']) && !empty($_GET['txtPassword']))
+	{
+		$txtUsername = $_GET['txtUsername'];
+		$txtPassword = $_GET['txtPassword'];
+		if ($con) {
+			$result = mysqli_query($con,"Select * from user where username ='".$txtUsername."' and password ='".$txtPassword."'");
+			$row = mysqli_fetch_array($result);
+			if (!empty($row))
+			{
+				header('Location: index.php?txtUsername='.$txtUsername);
+			}
+			else
+			{
+				echo "Not OK!";
+			}
+		}
+	}
+
+	mysqli_close($con);
+
 ?>
