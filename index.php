aa12c5785464194e8259883d8609f1775a58f5f9
batoan <batoan1221@gmail.com>
2013-10-26 15:47:10 +0700

    Fetch data from table

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php
-	// Create connection
-	$con=mysqli_connect("localhost","root","","day1");
+	function connectToDatabase( $hostName, $databaseName, $username, $password)
+	{
+		// Create connection
+		$con=mysqli_connect($hostName, $username, $password, $databaseName);
 
-	// Check connection
-	if (mysqli_connect_errno($con))
-	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		// Check connection
+		if (mysqli_connect_errno($con))
+		{
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+		else
+		{
+			// echo "Connect successfully";
+		}
+
+		return $con;
+	};
+
+	$con = connectToDatabase("localhost","day1","root","");
+	if ($con) {
+		$result = mysqli_query($con,"Select * from message");
+		while($row = mysqli_fetch_array($result))
+		{
+			echo $row['message_id'] . " " . $row['message_content'];
+			echo "<br>";
+		}
 	}
-	else
-	{
-		echo "Connect successfully";
-	}
+
+	mysqli_close($con);
 ?>
